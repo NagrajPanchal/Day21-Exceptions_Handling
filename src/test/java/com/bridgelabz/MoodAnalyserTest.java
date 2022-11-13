7e3623df181e4b0397f2e2009eae836cd28f5dc9
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-13 12:32:52 +0530

    Added : NULL or Empty Mood throw Custom Exception MoodAnalysisException

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,29 +1,23 @@
-// UC2 : Handle Exception if User Provides Invalid Mood
+// UC3-NULL or Empty Mood throw Custom Exception MoodAnalysisException
 
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class MoodAnalyserTest {
-
+public class MoodAnalyserTest
+{
     @Test
-    /*
-    Giving message using constructor of MoodAnalyser
-     */
-    public void givenMessage_WhenSad_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("Sad", mood);
-    }
-
-    @Test
-
-    public void givenMessage_WhenNotSad_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy mood");
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("Happy", mood);
-
-
+    public void givenNullMood_ShouldThrowException()
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        try
+        {
+            moodAnalyser.analyseMood(null);
+        }
+        catch (MoodAnalysesException e)
+        {
+            Assertions.assertEquals("Please enter proper message", e.getMessage());
+        }
     }
 }
